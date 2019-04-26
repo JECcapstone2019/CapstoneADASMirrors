0f2ff6992c3d7d7f38b4e8c4f0bc249e63cb09c1
EmilioQ <quaggioe@uwindsor.ca>
2019-04-25 23:31:09 -0400

    Fixed prefix/suffix issue with leftover _'s

--- tools/time_stamping.py
+++ tools/time_stamping.py
@@ -8,8 +8,14 @@ def getTimeStampedString():
     time_stamp = "%i_%i_%i-%i_%i_%i" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
     return time_stamp
 
+# Easy method for creating a time stamped folder somewhere - will not overwrite previous folders
 def createTimeStampedFolder(pathToFolder, str_Prefix='', str_Suffix=''):
-    folder_name = '%s_%s_%s' % (str_Prefix, getTimeStampedString(), str_Suffix)
+    folder_name = ''
+    if not(str_Prefix is ''):
+        folder_name = '%s_' % (str_Prefix)
+    folder_name += getTimeStampedString()
+    if not(str_Suffix is ''):
+        folder_name += '_%s' % str_Suffix
     path = os.path.join(pathToFolder, folder_name)
     if not os.path.exists(path):
         os.mkdir(path)
