20ded27dc327cb21f355ad7e707b3e0c1517d0e6
EmilioQ <quaggioe@uwindsor.ca>
2019-05-15 12:19:00 -0400

    fixed some problems that were due to my not so good use of pointers

--- arduino/arduino_control/arduino_control.ino
+++ arduino/arduino_control/arduino_control.ino
@@ -4,24 +4,33 @@
 const int BAUD_RATE = 9600;
 const int REFRESH_DELAY = 5; // in ms
 const int TIMEOUT = 50; // in ms
-const int MSG_READY = 3; // This will contain the msg header, cmdID, and size
 
-// Communication with Python constants
+// Message Length Definitions
+const int LEN_MSG_HEADER = 3;
+const int LEN_MSG_FOOTER = 1;
+
+// Other Message Definitions
 const byte MSG_HEADER = 0x7f;
 const byte MSG_FOOTER = 0xa5;
 
+const byte EMPTY = 0x00;
+
+// Message Index Definitions
 const int MSG_HEADER_IND = 0;
 const int MSG_CMD_IND = 1;
 const int MSG_SIZE_IND = 2;
 
-// Command Definitions
+// Command ID Definitions
 const int CMD_COMPLETED = 0x01;
 const int CMD_ACK = 0x02;
 const int CMD_LIDAR_READ_REG = 0x03;
 const int CMD_LIDAR_WRITE_REG = 0x04;
+const int CMD_NOP = 0x05;
 
-// Data Return Definitions
-const int CMD_LIDAR_READ_DATA = 0x01;
+// Data Return ID Definitions
+const int COMPLETE_NO_ERROR = 0x00;
+const int COMPLETE_UNRECOGNIZED_CMD = 0x01;
+const int COMPLETE_LIDAR_READ_DATA = 0x03;
 
 // Ack Errors
 const int ACK_NO_ERROR = 0x00;
@@ -32,117 +41,135 @@ const int ACK_TIMEOUT_ERROR = 0x04;
 
 // Completed Errors
 
-// Command Functions
+// Command Functions ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Do nothing and send a completed message with no error
+void cmd_nop(int data_footer_size, byte *message_data_footer){
+    sendCompletedMessage(COMPLETE_NO_ERROR, 1, EMPTY);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// Serial Functions
+// Serial Functions ////////////////////////////////////////////////////////////////////////////////////////////////////
 void emptySerialBuffer(){
     // Read until no more bytes in the buffer
     while (Serial.read() >= 0);
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-byte* createMessage(int cmd_id, byte *data){
-    int data_length = sizeof(data) + 1;
-    int message_length = MSG_READY + data_length;
+
+// Message Creation Functions //////////////////////////////////////////////////////////////////////////////////////////
+// Create a byte message for sending to the host and put it in the message[] buffer
+void sendMessage(int cmd_id, int data_size, byte *data){
+    int message_length = LEN_MSG_HEADER + data_size + LEN_MSG_FOOTER;
     byte message[message_length];
     message[0] = MSG_HEADER;
     message[1] = byte(cmd_id);
     message[2] = data_length;
-    message[message_length - 1] = MSG_FOOTER;
+    message[message_length - LEN_MSG_FOOTER] = MSG_FOOTER;
     for(int data_ind = 0; data_ind < sizeof(data); data_ind++){
-            message[data_ind + MSG_READY] = data[data_ind];
+            message[data_ind + LEN_MSG_HEADER] = data[data_ind];
     }
-    byte *msg_handle = message;
-    return msg_handle;
+    Serial.write(message, message_length);
+    return;
 }
 
-byte* createAckMessage(int rCode){
+// Create an ack message by giving it the code
+void sendAckMessage(int rCode){
     byte* code = byte(rCode);
-    return createMessage(CMD_ACK, code);
+    sendMessage(CMD_ACK, 1, code);
+    return;
 }
 
-byte* createCompletedMessage(int rCode, byte* data){
-    int data_length = sizeof(data);
+// Create a completed message by giving it the code and data associated if any
+void sendCompletedMessage(int rCode, int data_length, byte* data){
     byte code = byte(rCode);
-    byte new_data[data_length + 1];
+    byte new_data[1 + data_length];
     new_data[0] = code;
     for(int i=0; i< data_length; i++){
         new_data[i + 1] = data[i];
     }
     byte *data_handle = new_data;
-    return createMessage(CMD_COMPLETED, new_data);
+    createMessage(CMD_COMPLETED, data_length, data_handle);
+    return;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 // Parse the message to see what it wants us to do
-byte* parseMessage(int cmd_id, byte *message_data){
-    byte *completed_msg;
+void parseMessage(int cmd_id, int data_footer_size, byte *message_data_footer){
     if(cmd_id == CMD_LIDAR_READ_REG){
     }
     else if(cmd_id == CMD_LIDAR_WRITE_REG){
     }
+    else if(cmd_id == CMD_NOP){
+        cmd_nop();
+    }
     else{
         // Unrecognized Command ID - Return Error
-        completed_msg = &MSG_FOOTER;
+        sendCompletedMessage(COMPLETE_UNRECOGNIZED_CMD, 1, EMPTY);
     }
-    return completed_msg;
 }
 
-byte* removeFooter(byte* message_data){
-    int data_length = sizeof(message_data);
-    byte new_data[data_length - 1];
-    for(int i=0; i < data_length - 1; i++){
-        new_data[i] = message_data[i];
-    }
-    byte *data_handle = new_data;
-    return data_handle;
-}
-
+// Setup the serial communications
 void setup() {
     Wire.begin();
     Serial.begin(BAUD_RATE);
 }
 
 void loop() {
+    // byte array for containing the message header
+    byte message_header[LEN_MSG_HEADER];
+    // Pointer to the array containing the rest of the data
     byte *message;
+    Serial.write('0');
+    int msg_send_length;
+    byte* ack_msg;
+
     // Check if there is a full instruction set available
     int bytes_available = Serial.available();
-    if(bytes_available >= MSG_READY){
+    if(bytes_available >= LEN_MSG_HEADER){
         bool msg_completed = false;
         // Received a message - lets check its parameters and see what we need
-        Serial.readBytes(message, MSG_READY);
-        if(message[MSG_HEADER_IND] != MSG_HEADER){
+        Serial.readBytes(message_header, LEN_MSG_HEADER);
+        if(message_header[MSG_HEADER_IND] != MSG_HEADER){
             // Invalid termination, something is wrong with this message
-            byte* ack_msg = createAckMessage(ACK_HEADER_ERROR);
-            Serial.write(*ack_msg);
+            Serial.write('1');
+            ack_msg = createAckMessage(ACK_HEADER_ERROR);
+            msg_send_length = LEN_MSG_HEADER + ack_msg[MSG_SIZE_IND];
+            Serial.write(*ack_msg, msg_send_length);
             // Clear the buffer
             emptySerialBuffer();
         }
         else{
             // Check how many bytes we need for a full message
-            byte *message_data;
             int msg_size = (int) message[MSG_SIZE_IND];
             // Timeout at 50ms
             for(int delay_wait = 0; delay_wait < (TIMEOUT/REFRESH_DELAY); delay_wait++){
+                Serial.write('2');
                 // check if we have the full message now
                 bytes_available = Serial.available();
                 if(bytes_available == msg_size){
                     // All the bytes should be here now
-                    Serial.readBytes(message_data, bytes_available);
+                    byte message_data_footer[msg_size];
+                    Serial.readBytes(message_data_footer, msg_size);
+                    message &= message_data_footer;
                     msg_completed = true;
-                    int cmd_id = (int) message[MSG_CMD_IND];
+                    int cmd_id = (int) message_header[MSG_CMD_IND];
                     // Check the footer
-                    if(message[MSG_READY + msg_size - 1] == MSG_FOOTER){
+                    if(message[msg_size - LEN_MSG_FOOTER] == MSG_FOOTER){
                         // Send an ack back saying we got the message
-                        byte* ack_msg = createAckMessage(ACK_NO_ERROR);
+                        ack_msg = createAckMessage(ACK_NO_ERROR);
+                        msg_send_length = LEN_MSG_HEADER + ack_msg[MSG_SIZE_IND];
                         Serial.write(*ack_msg);
                         // Remove the footer and then ship off the data to the parser
-                        message_data = removeFooter(message_data);
-                        byte *completed_msg = parseMessage(cmd_id, message_data);
-                        Serial.write(*completed_msg);
+                        parseMessage(cmd_id, msg_size, message);
                         break;
                     }
                     else{
-                        byte* ack_msg = createAckMessage(ACK_FOOTER_ERROR);
+                        ack_msg = createAckMessage(ACK_FOOTER_ERROR);
+                        msg_send_length = LEN_MSG_HEADER + ack_msg[MSG_SIZE_IND];
                         Serial.write(*ack_msg);
                     }
                 }
@@ -152,7 +179,8 @@ void loop() {
                 }
             }
             if(msg_completed == false){
-                byte* ack_msg = createAckMessage(ACK_TIMEOUT_ERROR);
+                ack_msg = createAckMessage(ACK_TIMEOUT_ERROR);
+                msg_send_length = LEN_MSG_HEADER + ack_msg[MSG_SIZE_IND];
                 Serial.write(*ack_msg);
             }
             // Clear the buffer
