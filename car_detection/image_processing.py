f9110d91bd3d7adb5e7b305ffb20244d814384ab
jshamoon <shamoonj@uwindsor.ca>
2019-06-13 17:27:28 -0400

    no message

--- car_detection/image_processing.py
+++ car_detection/image_processing.py
@@ -1,10 +1,13 @@
 from multiprocessing import Queue, Pipe, Process
+import
 
 class ImageProcessor:
     def __init__(self, imageQueue, dataPipe):
         self.car_detected = False
         self.car_tracking = False
-        self.car_position = (-1, -1)
+        self.roi_updated = 1 # lets us know if there was enough of a change in the roi to display
+        self.car_position [-1, -1, -1, -1]
+        self.tracker = None
 
         self.image_queue = imageQueue
         self.data_pipe = dataPipe
@@ -40,10 +43,41 @@ class ImageProcessor:
                 self.runCarDetection()
 
     def runTracking(self):
-        pass
+        threshold = 0 # chnage after we experiment a bit
+        # update tracker
+        bool_updated, box = tracker.update(frame)
+        # check to see if new roi was found
+        if bool_updated == True:
+            # car was found
+            (x, y, w, h) = [int(i) for i in box] # box holds new roi coordinates
+            # check to see if change in roi is greater then theshold
+            delta = abs(car_position[0] - x) + abs(car_position[1] - y)
+            if delta > threshold:
+                self.roi_updated = 1
+            else:
+                self.roi_updated = -1
+            self.car_position = [x,y,x+w,y+w]
+        else:
+            # car was not found
+            carLost()
+
+
 
     def runCarDetection(self):
+        if self.car_detected:
+            self.tracker = cv2.TrackerKCF_create()
+            self.tracker.init(getFrame(), car_position)
         pass
+
+    # will reset all nessasary flags
+    def carLost(self):
+        self.car_position = [-1, -1, -1, -1]
+        self.car_detected = False
+        self.car_tracking = False
+        self.roi_updated = 1
+        self.tracker = None
+
+
 
 
 if __name__ == '__main__':
