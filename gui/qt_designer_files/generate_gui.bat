a9e55476146c4e7a00f3eec10ccbd3de611ba300
EmilioQ <quaggiotto.e@gmail.com>
2019-06-18 19:58:27 -0400

    added first gui files

--- gui/qt_designer_files/generate_gui.bat
+++ gui/qt_designer_files/generate_gui.bat
@@ -1,6 +1,10 @@
 cd ..
 cd ..
-virtenv/Scripts/activate.bat
-cd gui/qt_designer_files
-python -m PyQt5.uic.pyuic mirrorless_mirrors.ui -o ../main_gui.py
-deactivate
+cd virtenv
+cd Scripts
+call activate
+cd ..
+cd ..
+cd gui\qt_designer_files
+python -m PyQt5.uic.pyuic mirrorless_mirrors.ui -o main_gui_ui.py
+pause
