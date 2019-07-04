0cbea00c34989535e680b464d82f9cae3731b5b8
EmilioQ <quaggiotto.e@gmail.com>
2019-07-04 18:40:01 -0400

    sketchy working viewer thread

--- gui/main_gui.py
+++ gui/main_gui.py
@@ -1,18 +1,16 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-from gui.qt_designer_files import main_gui_ui
+from gui.qt_designer_files import main_gui_ui, viewer_thread
 import sys
 
 # Short overriding class for running the application
 class runnerWindow(QtWidgets.QMainWindow, main_gui_ui.Ui_MainWindow):
     def __init__(self):
-        # Explaining super is out of the scope of this article
-        # So please google it if you're not familar with it
-        # Simple reason why we use it here is that it allows us to
-        # access variables, methods etc in the design.py file
         super(self.__class__, self).__init__()
         self.setupUi(self)  # This is defined in design.py file automatically
                             # It sets up layout and widgets that are defined
         self.connectObjectFunctions()
+        self.image_thread = viewer_thread.ImageViewingThread(imageQueue=None, parent=self.imageViewer)
+        self.image_thread.start()
 
     # connect all the buttons ect to their respective functions
     def connectObjectFunctions(self):
--- gui/qt_designer_files/viewer_thread.py
+++ gui/qt_designer_files/viewer_thread.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import QThread, pyqtSignal, Qt
 from PyQt5.QtGui import QPixmap, QImage
 import datetime
 
-class Thread(QThread):
+class ImageViewingThread(QThread):
     changePixmap = pyqtSignal(QPixmap)
     changeLabel = pyqtSignal(str)
 
@@ -20,17 +20,22 @@ class Thread(QThread):
 
     def run(self):
         while self.isRunning:
-            frame = self.grabFrame()
-            #color_frame = frames.get_color_frame()
-            #rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            rgbImage = frame
-            convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
-            convertToQtFormat = QPixmap.fromImage(convertToQtFormat)
-            p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
-            self.changePixmap.emit(p)
-            now = datetime.datetime.now()
-            sec = now.second
-            self.changeLabel.emit(str(sec))
+            try:
+                frame = self.grabFrame()
+                #color_frame = frames.get_color_frame()
+                #rgbImage = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                rgbImage = frame
+                convertToQtFormat = QImage(rgbImage.data, rgbImage.shape[1], rgbImage.shape[0], QImage.Format_RGB888)
+                convertToQtFormat = QPixmap.fromImage(convertToQtFormat)
+                p = convertToQtFormat.scaled(640, 480, Qt.KeepAspectRatio)
+                self.changePixmap.emit(p)
+                now = datetime.datetime.now()
+                sec = now.second
+                self.changeLabel.emit(str(sec))
+            except:
+                pass
+            print("Running...")
+        print("Thread exited")
 
     def stop(self):
         self.isRunning = False
